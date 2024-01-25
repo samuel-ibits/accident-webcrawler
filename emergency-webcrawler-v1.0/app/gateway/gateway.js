3f0b1232ba4f592bfef1a9bbb96aa6baf449496e
samuel-ibits <ibitoyesamuel25@gmail.com>
2024-01-25 11:52:08 +0100

    create threads

--- emergency-webcrawler-v1.0/app/gateway/gateway.js
+++ emergency-webcrawler-v1.0/app/gateway/gateway.js
@@ -1,18 +1,26 @@
 import { exec } from "child_process";
+import initiateSearchBarWorker from "../../utils/workers/searchbars.js";
+
 
 export default class Gateways {
   constructor() {}
-async engine(request){
-  switch(request){
-    case "":
-      request()
-  }
-}
-  async type1(request) {
-    const { text } = await request.json();
+
+  async google(request) {
+    //get the data
+    const { emergencyType, fromDate, toDate, specialParameters, searchBase } =
+      await request;
+
+    const text =
+      "new request" +
+      emergencyType +
+      specialParameters +
+      "from:" +
+      fromDate +
+      "to:" +
+      toDate;
     const translatedTextPromise = new Promise((resolve, reject) => {
       exec(
-        `source virenv/bin/activate && python3 translate.py"${text}"`,
+        `cd crawlers/python && python -m venv scrape_venv && scrape_venv/Scripts/activate && python -m pip install -r requirments.txt && python google.py"${text}"`,
         (error, stdout, stderr) => {
           if (error) {
             console.error(`exec error:${error}`);
@@ -26,8 +34,32 @@ async engine(request){
 
     return translatedText;
   }
+
+  async searchBar(request) {
+   
+//get the data
+    const { emergencyType, fromDate, toDate, specialParameters, searchBase } =
+      await request;
+
+  
+     await initiateSearchBarWorker({
+       emergencyType,
+       fromDate,
+       toDate,
+       specialParameters,
+       searchBase,
+     });
+
+    return;
+  }
+
+
+
+
+
+
   async test(request) {
-    console.log({message:"got to gateway sucessfully", data: request})
+    console.log({ message: "got to gateway sucessfully", data: request });
     return request;
   }
 }
